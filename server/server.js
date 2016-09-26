a4b1fcffef67c5ff98a2154f7cbc05171dd76b82
John Hamm <johndhamm1@gmail.com>
2016-09-26 10:58:00 -0500

    bootstrap added

--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ const port = process.env.PORT || 3000
 //middlewares
 app.use(express.static('client')) //client folder
 
+app.get('/api/title', (req, res) =>
+	res.send({ title: 'MEAN 101 from Node' })
+)
+
 app.listen(port, () => console.log(`listening on port: ${port}`))
