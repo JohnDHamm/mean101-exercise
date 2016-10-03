53fa3de1546e8839f4156d52497f9754fe44f057
John Hamm <johndhamm1@gmail.com>
2016-10-03 10:20:24 -0500

    Initial websocket implementation

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const { json } = require('body-parser')
+const { Server } = require('http');
+const socketio = require('socket.io');
 const express = require('express');
 const mongoose = require('mongoose')
 
+const app = express();
+const server = Server(app); //creates second http server that listens to websockets
+const io = socketio(server); //
+
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
 const PORT = process.env.PORT || 3000
-
-const app = express();
 
 //middlewares
 app.use(express.static('client')) //client folder
@@ -27,32 +31,26 @@ app.get('/api/messages', (req, res, err) =>
 		.find()
 		.then(messages => res.json( {messages} ))
 		.catch(err)
-	// res.json({ //hard coding to test
-	// 	messages: [
-	// 		{
-	// 			author: 'John',
-	// 			content: 'Anyone here?!'
-	// 		},
-	// 		{
-	// 			author: 'Jill',
-	// 			content: 'nope'
-	// 		}
-	// 	]
-	// })
 )
 
 app.post('/api/messages', (req, res, err) => {
 	const msg = req.body
 	Message
 		.create(msg)
-		.then(msg => res.json(msg))
+		.then(msg => res.status(201).json(msg))
 		.catch(err)
 	}
 )
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URL, () =>
-	app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
+	// app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
+	server.listen(PORT, () => console.log(`listening on port: ${PORT}`))
 )
+
+io.on('connection', socket => {
+	console.log(`Socket connected: ${socket.id}`)
+	socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
+})
 
 
